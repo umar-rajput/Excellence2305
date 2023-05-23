83f2c282a1a3f871c06a21f69de78652c4aefd63
unknown <aman.sahu@fluper.in>
2023-05-23 18:58:32 +0530

    method event

--- app.js
+++ app.js
@@ -19,14 +19,22 @@ const app = Vue.createApp({
             seen:true,
             url:"www.google.com",
             isActive:true,
-            buttonText:"Click"
+            buttonText:"Click",
+
         }
     },
     methods:{ 
         doSomething(){
             document.getElementById("changeColor").style.color="red";
             this.buttonText="Clicked";
-        }
+        },
+        greet(event) {
+            alert(`Hello ${this.name}!`)
+
+            if (event) {
+              alert(event.target.tagName)
+            }
+          }
     }
 
 
